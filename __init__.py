e9dc64e3773090b3a4d4e7e01db9accc82998f74
Alessandro Zomparelli <alessandro.zomparelli@gmail.com>
2020-01-14 01:12:49 +0300

    changed version number

--- __init__.py
+++ __init__.py
@@ -33,7 +33,7 @@
 bl_info = {
     "name": "Tissue",
     "author": "Alessandro Zomparelli (Co-de-iT)",
-    "version": (0, 3, 38),
+    "version": (0, 3, 39),
     "blender": (2, 80, 0),
     "location": "",
     "description": "Tools for Computational Design",
